89dd750b2d841411adb228ced11d984629eb1cb4
Hjalmar O. Fjøsne <fjoesne@gmail.com>
2020-12-22 14:21:50 +0100

    ref before assignment on line 78

--- 2020/day22.py
+++ 2020/day22.py
@@ -12,12 +12,12 @@ for card in decks:
     else:
         p += 1
 
-def score(winners_deck):
+def score(winner_deck):
     points = 0
     i = 1
     # print('winners deck: %s' %(winners_deck))
-    while len(winners_deck) > 0:
-        card = winners_deck.pop()
+    while len(winner_deck) > 0:
+        card = winner_deck.pop()
         points += i * card
         # print('card(%s): %s, score: %s' %(i, card, points))
         i += 1
@@ -49,15 +49,35 @@ def play_round(deck_a, deck_b):
     
     return winning_deck
 
-# deck_series = []
-# def play_recursive_combat(deck_a, deck_b):
-#     i = 0
-#     if deck_a or deck_b in deck_series:
-#         print('Recursed! player 1 wins!')
-#         return False
-#     while min(len(deck_a), len(deck_b)) > 0:
-#         if deck_a[0] == len(deck_a):
-#         play_recursive_combat(play_round(deck_a, deck_b))
+player_one_deck_series = []
+player_two_deck_series = []
+def play_recursive_combat(deck_a, deck_b):
+    i = 0
+    if deck_a in player_one_deck_series or deck_b in player_two_deck_series:
+        print('Recurse rule! player 1 wins!')
+        winning_deck = deck_a
+    while min(len(deck_a), len(deck_b)) > 0:
+        da = deck_a.pop(0)
+        db = deck_b.pop(0)
+        if db > len(deck_b) and da > len(deck_a):
+            winning_deck = play_recursive_combat(deck_a, deck_b)
+        elif da > db:
+            deck_a.append(da)
+            deck_a.append(db)
+            if len(deck_b) == 0:
+                print('deck a is winner!')
+                winning_deck = deck_a
+        else:
+            deck_b.append(db)
+            deck_b.append(da)
+            if len(deck_a) == 0:
+                print('deck b is winner!')
+                winning_deck = deck_b
+        i += 1
+
+    return winning_deck
 
 
 print(score(play_round(player_one_deck, player_two_deck)))
+
+play_recursive_combat(player_one_deck, player_two_deck)
