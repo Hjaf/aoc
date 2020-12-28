1b47dacd099fe8364633ad86f2f873c3bb5e4485
Hjalmar O. Fjøsne <fjoesne@gmail.com>
2020-12-28 14:39:25 +0100

    solve day 1 part 1

--- 2020/day1.py
+++ 2020/day1.py
@@ -1,4 +1,11 @@
+# inumbers = open('day1inputsample.txt','r').readlines()
 inumbers = open('day1input.txt','r').readlines()
 
-for num in inumbers:
-    print(num.strip())
+for rnum in inumbers:
+    num = int(rnum.strip())
+    for ronum in inumbers:
+        onum = int(ronum.strip()) 
+        num_sum = num + onum
+        if num_sum == 2020 and onum != num:
+            onum_sum = num * onum
+            print('numbers: %s + %s = %s \nmultiplied: %s' % (num, onum, num_sum, onum_sum))
