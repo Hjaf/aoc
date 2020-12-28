12ee6100fe927b866c2c1eb58a35ab3c65983326
Hjalmar O. Fjøsne <fjoesne@gmail.com>
2020-12-28 16:02:08 +0100

    solve day 1 part one and two

--- 2020/day1.py
+++ 2020/day1.py
@@ -1,10 +1,6 @@
-# inumbers = open('day1inputsample.txt','r').readlines()
 inumbers = open('day1input.txt','r').readlines()
 
 target_value = 2020
-
-# def candidates(expense):
-
 expenses = []
 answer = False
 
@@ -20,25 +16,25 @@ for rnum in inumbers:
             print('numbers: %s + %s = %s \nmultiplied: %s' % (num, onum, num_sum, answer))
            
 def product_three(x):
-    r_expenses = expenses
+    r_expenses = list(expenses)
     r_expenses.pop(r_expenses.index(x))
-    for y in expenses:
+    for y in r_expenses:
         if x + y < target_value:
-            print(len(expenses))
             r_expenses.pop(r_expenses.index(y))
             product = x + y
             remaining = target_value - product
             # print(remaining)
             if remaining in r_expenses:
                 answer = x * y * remaining 
-                print('%s * %s * %s = %s'%(x, y, product, answer))
-                return answer
+                return str.format('%s * %s * %s = %s'%(x, y, product, answer))
+                # break
             else:
-                r_expenses = expenses
+                r_expenses.append(y)
 
             
 
 for expense in expenses:
     result = product_three(expense)
     if result:
-        print('multiplication of 3 values with sum 2020: %s' %(result))
+        print('multiplication of 3 values with sum 2020:\n %s' %(result))
+        break
