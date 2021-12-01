b41bcd4959de73484a3b18a23d4c88022b5b429f
Hjalmar Otto Fjøsne <Hjalmar.Fjosne@akersolutions.com>
2021-12-01 14:06:42 +0100

    -

--- 2021/day1.py
+++ 2021/day1.py
@@ -10,13 +10,8 @@ dcg = dn[1:3]
 for d in dn:
     if i > 2:
         dpg = dn[i-3:i]
-<<<<<<< HEAD
         dcg = dn[i-2:(i+1)]
         if (sum(dpg) < sum(dcg)):
-=======
-        iCurrentGroup = dn[i-2:(i+1)]
-        if (sum(dpg) < sum(iCurrentGroup)):
->>>>>>> d7c7175868c039bb8148ad0701bd7a77e69fdef1
             agic += 1
     if (d > dn[i-1]):
         dic += 1      
