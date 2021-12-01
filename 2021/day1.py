d7c7175868c039bb8148ad0701bd7a77e69fdef1
Hjalmar Otto Fjøsne <Hjalmar.Fjosne@akersolutions.com>
2021-12-01 14:00:15 +0100

    solve day 1 part 1 and 2

---
+++ 2021/day1.py
@@ -0,0 +1,22 @@
+input = open('input/day1input.txt','r').readlines()
+dn = []
+for s in input:
+    dn.append(int(s))
+i = 0
+dic = 0
+agic = 0
+dpg = dn[0:2]
+dcg = dn[1:3]
+for d in dn:
+    if i > 2:
+        dpg = dn[i-3:i]
+        iCurrentGroup = dn[i-2:(i+1)]
+        if (sum(dpg) < sum(iCurrentGroup)):
+            agic += 1
+    if (d > dn[i-1]):
+        dic += 1      
+    i += 1
+print(f'''
+Part 1: {dic}
+Part 2: {agic}
+''')
